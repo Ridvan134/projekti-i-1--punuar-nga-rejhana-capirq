22eccca458d9dc73830919f53e9d65b7495a53ed
Ridvan134 <76845503+Ridvan134@users.noreply.github.com>
2021-06-10 19:37:15 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.showString("Hello!")
+basic.forever(function () {
+    basic.showString("Une jam Rejhana nga klasa 6")
+    basic.pause(5000)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Heart)
+    basic.showString("Faleminderit")
+})
